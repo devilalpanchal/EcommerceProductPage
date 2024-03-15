94be45a71bb2e7b7ccc5140b43676da5d0ac82cb
Devilal panchal <devilalpanchal551@gmail.com>
2024-03-15 22:12:07 +0530

    123

--- function.js
+++ function.js
@@ -18,7 +18,7 @@ const Simage02 = document.querySelector('#Simage02')
 const Simage03 = document.querySelector('#Simage03')
 const Simage04 = document.querySelector('#Simage04')
 
-// to increase & decrese****************
+// to increase & decrese*****
 let count = 0
 minus.addEventListener('click', () => {
     count--
@@ -27,7 +27,7 @@ minus.addEventListener('click', () => {
     division.innerHTML = "  "
     addCart.innerHTML = count
     addCart.append(division)
-    Multiply125.innerHTML =   count * -125
+    Multiply125.innerHTML = count * -125
     incresePop.innerHTML = count
 })
 
@@ -38,17 +38,17 @@ plus.addEventListener('click', () => {
     division.innerHTML = "  "
     addCart.innerHTML = count
     addCart.append(division)
-    Multiply125.innerHTML =   count * 125
-    incresePop.innerHTML = count 
-})
-trash.addEventListener('click',()=>{
-    count = 0
-    Multiply125.innerHTML =   count * 0
+    Multiply125.innerHTML = count * 125
     incresePop.innerHTML = count
-HideEmpety.classList.add('hide')
-empty.classList.remove('hide')
-addCart.classList.add('hide')
-    
+})
+trash.addEventListener('click', () => {
+    count = 0
+    Multiply125.innerHTML = count * 0
+    incresePop.innerHTML = count
+    HideEmpety.classList.add('hide')
+    empty.classList.remove('hide')
+    addCart.classList.add('hide')
+
 })
 
 addCartBtn.addEventListener('click', () => {
@@ -60,29 +60,28 @@ addCartBtn.addEventListener('click', () => {
 })
 
 // to image change*********
-Simage01.addEventListener('click',()=>{
+Simage01.addEventListener('click', () => {
     img01.classList.toggle('hide')
     img02.classList.add('hide')
     img03.classList.add('hide')
     img04.classList.add('hide')
-    // document.location.reload();
 })
 
-Simage02.addEventListener('click',()=>{
+Simage02.addEventListener('click', () => {
     img02.classList.toggle('hide')
     img01.classList.add('hide')
     img03.classList.add('hide')
     img04.classList.add('hide')
 })
 
-Simage03.addEventListener('click',()=>{
+Simage03.addEventListener('click', () => {
     img03.classList.toggle('hide')
     img01.classList.add('hide')
     img02.classList.add('hide')
     img04.classList.add('hide')
 })
 
-Simage04.addEventListener('click',()=>{
+Simage04.addEventListener('click', () => {
     img04.classList.toggle('hide')
     img01.classList.add('hide')
     img02.classList.add('hide')
@@ -93,7 +92,7 @@ Simage04.addEventListener('click',()=>{
 const AddCartimgOfAvatarImage = document.querySelector('.imgOfAvatarImage')
 const popup = document.querySelector('.popup')
 const content = document.querySelector('.content')
-AddCartimgOfAvatarImage.addEventListener('click',()=>{
+AddCartimgOfAvatarImage.addEventListener('click', () => {
     popup.classList.toggle('hide')
 })
 
@@ -106,28 +105,28 @@ const ppimage2 = document.querySelector('#ppimage2')
 const ppimage3 = document.querySelector('#ppimage3')
 const ppimage4 = document.querySelector('#ppimage4')
 
-Simage01.addEventListener('click',()=>{
+Simage01.addEventListener('click', () => {
     ppimage1.classList.toggle('hide')
     ppimage2.classList.add('hide')
     ppimage3.classList.add('hide')
     ppimage4.classList.add('hide')
 })
 
-Simage02.addEventListener('click',()=>{
-ppimage2.classList.remove('hide')
-ppimage1.classList.add('hide')
-ppimage3.classList.add('hide')
-ppimage4.classList.add('hide')
+Simage02.addEventListener('click', () => {
+    ppimage2.classList.remove('hide')
+    ppimage1.classList.add('hide')
+    ppimage3.classList.add('hide')
+    ppimage4.classList.add('hide')
 })
 
-Simage03.addEventListener('click',()=>{
+Simage03.addEventListener('click', () => {
     ppimage3.classList.remove('hide')
     ppimage1.classList.add('hide')
     ppimage2.classList.add('hide')
     ppimage4.classList.add('hide')
 })
 
-Simage04.addEventListener('click',()=>{
+Simage04.addEventListener('click', () => {
     ppimage4.classList.remove('hide')
     ppimage1.classList.add('hide')
     ppimage2.classList.add('hide')
@@ -135,14 +134,14 @@ Simage04.addEventListener('click',()=>{
 })
 
 
-const threeLine =  document.querySelector('.threeLine')
-const name1 =  document.getElementsByClassName('.name')
-const remove =  document.getElementsByClassName('.remove')
-threeLine.addEventListener('click',()=>{
+const threeLine = document.querySelector('.threeLine')
+const name1 = document.getElementsByClassName('.name')
+const remove = document.getElementsByClassName('.remove')
+threeLine.addEventListener('click', () => {
     name1.classList.toggle('hide')
-console.log('devilal pancha;l');
+    console.log('devilal pancha;l');
 })
-remove.addEventListener('click',()=>{
-name1.classList.add('hide')
+remove.addEventListener('click', () => {
+    name1.classList.add('hide')
 
 })
